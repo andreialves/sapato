ded04bb8ee1cbf4d32630709eeaf01fe9de82765
andreialves <andreialvesccp@gmail.com>
2021-05-20 23:17:23 -0300

    Correção de diretorio

--- font-end/e2e/src/app.e2e-spec.ts
+++ font-end/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('front-end app is running!');
+    expect(await page.getTitleText()).toEqual('font-end app is running!');
   });
 
   afterEach(async () => {
